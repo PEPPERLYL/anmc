7312c2a7c9e7dbdbebc78ac081305cacb45e0b02
kayode-victor <kayode.o.victor@gmail.com>
2024-02-17 14:22:05 +0100

    third commit-12

--- components/Attend.js
+++ components/Attend.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
+
 import attend01 from "../assets/attend01.png";
 import attend02 from "../assets/attend02.png";
 import attend03 from "../assets/attend03.png";
@@ -11,7 +14,6 @@ import attend09 from "../assets/attend09.png";
 import attend10 from "../assets/attend10.png";
 import attend11 from "../assets/attend11.png";
 import attend12 from "../assets/attend12.png";
-import Image from "next/image";
 
 const Attend = () => {
   const attendbox = [
@@ -70,24 +72,27 @@ const Attend = () => {
       <div className="mb-5 lg:mb-10">
         <p className="text-3xl font-medium text-center">WHO CAN ATTEND?</p>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-4 mx-10 gap-x-20 gap-y-10">
+      <div className="grid grid-cols-3 md:grid-cols-4 lg:mx-32 mx-3 gap-x-10 gap-y-5 lg:gap-x-20 lg:gap-y-10">
         {attendbox.map((item, index) => (
-          <div
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ ease: "easeOut", duration: 0.5, delay: index * 0.1 }}
             key={index}
-            className="relative overflow-hidden rounded-tl-3xl rounded-br-3xl shadow-md cursor-pointer group"
+            className="relative overflow-hidden rounded-tl-3xl rounded-br-3xl shadow-md cursor-pointer hover:-translate-y-2 group"
           >
             <Image
               src={item.image}
               alt={item.title}
               width={250}
               height={150}
-              className="object-cover w-full h-full  duration-300 ease-in-out "
+              className="object-cover w-full h-full"
             />
-            <div className="absolute inset-0 bg-black opacity-50 transition-opacity duration-300 ease-in-out group-hover:opacity-100"></div>
+            <div className="absolute inset-0 bg-black opacity-30 transition-opacity duration-300 ease-in-out group-hover:opacity-100"></div>
             <p className="absolute inset-0 flex items-center justify-center text-center text-white font-light text-xl pointer-events-none">
               {item.title}
             </p>
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
