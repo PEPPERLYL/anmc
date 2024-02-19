3be510960f9f6bfd5b313fccb619148b1eb1bf26
kayode-victor <kayode.o.victor@gmail.com>
2024-02-19 03:26:16 +0100

    fourth commit-1

--- components/Mission.js
+++ components/Mission.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import missionimg from "../assets/mission.png";
 import Image from "next/image";
 import wave from "../assets/wave.png";
@@ -7,14 +8,29 @@ const Mission = () => {
     <div className="relative flex">
       <div className="w-0/3 lg:w-2/6 h-[560px] bg-white"></div>
       <div className="w-full lg:w-2/3 h-[560px] bg-[#001E2C]"></div>
-      <div className="absolute inset-0 flex  bg-white lg:mx-32 lg:my-32 mx-10 my-24">
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ ease: "easeOut", duration: 0.5, delay: 0.3 }}
+        className="absolute inset-0 flex  bg-white lg:mx-32 lg:my-32 mx-10 my-24"
+      >
         <div className="w-full lg:w-3/4 flex flex-col items-start px-2">
-          <div className=" flex flex-col items-center py-4">
+          <motion.div
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ ease: "easeOut", duration: 0.5, delay: 0.6 }}
+            className=" flex flex-col items-center py-4"
+          >
             <p className="text-3xl font-semibold">OUR MISSION</p>
             <Image src={wave} alt="Wave" className="my-2 px-4" />
-          </div>
+          </motion.div>
           <div>
-            <p className="text-md">
+            <motion.p
+              initial={{ opacity: 0, y: 30 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ ease: "easeOut", duration: 0.5, delay: 0.9 }}
+              className="text-md"
+            >
               Empowering Africa’s digital potential.
               <br /> Uniting Minds and Fostering Innovation.
               <br /> Pushing Creativity and Collaboration in NFTs and Metaverse
@@ -24,7 +40,7 @@ const Mission = () => {
               Gaming, Blockchain, and Web.
               <br /> Providing training, education, and experience for the
               Africa Web3 Ecosystem
-            </p>
+            </motion.p>
           </div>
         </div>
         <div className="hidden lg:flex lg:w-1/4 h-full">
@@ -36,7 +52,7 @@ const Mission = () => {
             />
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
